fd8942d3a7c8c4c121dc3bd7e27921ca35252ad8
parthhacks <41590708+parthhacks@users.noreply.github.com>
2018-07-25 17:02:29 -0700

    Adding AppInsights Package (#33)

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,4 +1,4 @@
-/// <reference path="../../node_modules/monaco-editor/monaco.d.ts" />
+///<reference path="../../node_modules/monaco-editor/monaco.d.ts" />
 
 import { EditorComponent } from "./components/editor";
 import { RunComponent } from "./components/run";
@@ -10,6 +10,7 @@ import * as ReactDOM from "react-dom";
 import { HashRouter, Route, Switch, Redirect } from "react-router-dom";
 import { Provider } from "react-redux";
 import { Compilation } from "../compiler/compilation";
+import { AppInsights } from "applicationinsights-js";
 
 window.document.title = "SmallBasic-Online";
 
@@ -20,9 +21,20 @@ const initialState: AppState = {
         `name = TextWindow.Read()`,
         `TextWindow.WriteLine("Hello " + name + "!")`
     ].join("\n"))
-};
+
+ };
 
 const store = createStore(reduce, initialState);
+
+AppInsights.downloadAndSetup!({ instrumentationKey: "xxxx-xxx-xxx-xxx-xxxxxxx" });
+AppInsights.trackPageView(
+    "Small Basic HomePage", /* (optional) page name */
+    "http://localhost:8080", /* (optional) page url if available */
+    { prop1: "prop1", prop2: "prop2" }, /* (optional) dimension dictionary */
+    { measurement1: 1 }, /* (optional) metric dictionary */
+    60 /* page view duration in milliseconds */
+);
+AppInsights.trackEvent("TestEvent", { prop1: "prop1", prop2: "prop2" }, { measurement1: 1 });
 
 ReactDOM.render((
     <Provider store={store}>
